58f1c54291bc20652683946680002dc53c0eb532
chandan kumar <vivekans2016@gmail.com>
2021-06-30 17:29:42 +0530

    basic wroking done

--- app.js
+++ app.js
@@ -1,12 +1,140 @@
-const stockName=document.getElementById('stockName')
-const button=document.getElementById('button')
+const form = document.getElementById("input-form");
+const StockName = document.getElementById("StockName");
+const buy = document.getElementById("buy");
+const sell = document.getElementById("sell");
+const entryLevel = document.getElementById("entryLevel");
+const Target = document.getElementById("Target");
+const sl = document.getElementById("sl");
+const Quantity = document.getElementById("Quantity");
+const reason = document.getElementById("reason");
+const button = document.getElementById("button");
+const displayTable = document.getElementById("displayTable");
 
-button.onclick=()=>{
-    console.log(stockName.value )
-   const data=fetch(`http://localhost:4000/getData/:${stockName.value}`)
-   .then((response)=>response.json())
-   .then(data=>console.log(data))
-//    const jsonData=data.json();
-//    console.log(jsonData)
+let day = new Date().toLocaleDateString();
+
+const entries = JSON.parse(localStorage.getItem("allData"));
+//console.log(entries)
+//entries.map(entry=>console.log(entry.StockName))
+entries.map((entry) => {
+
+
+    const data = fetch(`http://localhost:4000/getData/:${entry.StockName}`)
+    .then((response) => response.json())
+    .then((data) => {
+        const tr = document.createElement("tr");
+            entry.Profit=data.High-entry.entryLevel
+            entry.NetProfit=entry.Profit*entry.Quantity
+            entry.Profit_percent=(entry.Profit/entry.entryLevel)*100;
+        tr.innerHTML = `
+          
+          <td>${entry.day}</td>
+          <td>${entry.StockName}</td>
+          <td>${entry.entryLevel}</td>
+          <td>${entry.Target}</td>
+          <td>${entry.sl}</td>
+          <td>${entry.Quantity}</td>
+          <td>${entry.High}</td>
+          <td>${entry.Low}</td>
+          <td>${entry.Cmp}</td>
+          <td>${entry.Remarks}</td>
+          <td>${entry.ExitPrice}</td>
+          <td>${entry.Profit}</td>
+          <td>${entry.NetProfit}</td>
+          <td>${entry.Profit_percent}</td>
+          <td>${entry.TradeReason}</td>
+          <td>${entry.Learning}</td>
+          
+          `;
+        displayTable.appendChild(tr);
+    });
+})
+//   const tr = document.createElement("tr");
+
+//   tr.innerHTML = `
     
-}
+//     <td>${entry.day}</td>
+//     <td>${entry.StockName}</td>
+//     <td>${entry.entryLevel}</td>
+//     <td>${entry.Target}</td>
+//     <td>${entry.sl}</td>
+//     <td>${entry.Quantity}</td>
+//     <td>${entry.High}</td>
+//     <td>${entry.Low}</td>
+//     <td>${entry.Cmp}</td>
+//     <td>${entry.Remarks}</td>
+//     <td>${entry.ExitPrice}</td>
+//     <td>${entry.Profit}</td>
+//     <td>${entry.NetProfit}</td>
+//     <td>${entry.Profit_percent}</td>
+//     <td>${entry.TradeReason}</td>
+//     <td>${entry.Learning}</td>
+    
+//     `;
+//   displayTable.appendChild(tr);
+// });
+
+form.onsubmit = (e) => {
+  e.preventDefault();
+  console.log(StockName.value);
+
+  const data = fetch(`http://localhost:4000/getData/:${StockName.value}`)
+    .then((response) => response.json())
+    .then((data) => {
+      addHTML(data.High, data.Low, data.LastRate);
+    });
+};
+
+addHTML = (High, Low, Cmp) => {
+  const tr = document.createElement("tr");
+
+  tr.innerHTML = `
+
+<td>${day}</td>
+<td>${StockName.value}</td>
+<td>${entryLevel.value}</td>
+<td>${Target.value}</td>
+<td>${sl.value}</td>
+<td>${Quantity.value}</td>
+<td>${High}</td>
+<td>${Low}</td>
+<td>${Cmp}</td>
+<td>${(Remarks = 0)}</td>
+<td>${(ExitPrice = 0)}</td>
+<td>${(Profit = 0)}</td>
+<td>${(NetProfit = 0)}</td>
+<td>${(Profit_percent = 0)}</td>
+<td>${(TradeReason = 0)}</td>
+<td>${(Learning = 0)}</td>
+
+`;
+
+  addToLocalStorage(High, Low, Cmp);
+  displayTable.appendChild(tr);
+};
+const addToLocalStorage = (High, Low, Cmp) => {
+  let existingData = JSON.parse(localStorage.getItem("allData"));
+  if (existingData == null) existingData = [];
+
+  let data = {
+    day: day,
+    StockName: StockName.value,
+    entryLevel: entryLevel.value,
+    Target: Target.value,
+    sl: sl.value,
+    Quantity: Quantity.value,
+    High: High,
+    Low: Low,
+    Cmp: Cmp,
+    Remarks: Remarks,
+    ExitPrice: ExitPrice,
+    Profit: Profit,
+    NetProfit: NetProfit,
+    Profit_percent: NetProfit,
+    TradeReason: TradeReason,
+    Learning: Learning,
+  };
+  localStorage.setItem("data", JSON.stringify(data));
+  // Save allEntries back to local storage
+  existingData.push(data);
+  localStorage.setItem("allData", JSON.stringify(existingData));
+};
--- backend/getData.js
+++ backend/getData.js
@@ -1,42 +1,48 @@
 const axios = require("axios");
+const dotenv=require('dotenv')
+
+dotenv.config()
+
+
 const Data=async(stockName)=>{
-    const config = {
-        head: {
-          appName: "5P57337945",
-          appVer: "1.0",
-          key: "N2dZgSSWmLxWPIygn1GBvP7nmBhe6kw1",
-          osName: "Android",
-          requestCode: "5PMF",
-          userId: "vKu3EXy1q8j",
-          password: "mxth0a6vTnw",
-        },
-        body: {
-          Count: 1,
-          MarketFeedData: [
-            {
-              Exch: "N",
-              ExchType: "C",
-              Symbol: `${stockName}`,
-              Expiry: "",
-              StrikePrice: "0",
-              OptionType: "",
-            },
-          ],
-        },
-      };
+ 
+    const config = 
+      {
+        "head": {
+          "appName": process.env.appName,
+          "appVer":process.env.appVer,
+          "key": process.env.key,
+          "osName":process.env.osName,
+          "requestCode": process.env.requestCode,
+          "userId": process.env.userId,
+          "password": process.env.password,
+      },
+        "body": {
+            "Count": 1,
+            "MarketFeedData":[{"Exch":"N",
+            "ExchType":"C","Symbol":`${stockName}`,"Expiry":"","StrikePrice":"0","OptionType":""}],
+            "ClientLoginType":0,"LastRequestTime":"/Date(0)/","RefreshRate":"H"}       
+        };
+      
       try{
         const result=await  axios
         .post("https://Openapi.5paisa.com/VendorsAPI/Service1.svc/MarketFeed", config)
-        const {High,Low,PClose}=result.data.body.Data[0]
-        //console.log(result.data.body)
-        // console.log({High,Low,PClose})
-        // res.send({High,Low,PClose})
+     // console.log(result)
+      //   console.log(result.data.body)
+      //  // console.log({High,Low,PClose})
+      //   res.send({High,Low,PClose})
+      const {High,Low,LastRate}=result.data.body.Data[0]
         if(result)
-        return ({High,Low,PClose})
+        {
+         
+          return ({High,Low,LastRate})
+        }
+       
       }
   catch(error){
 return (error)
   }
+//return (5)
       
    
 }
